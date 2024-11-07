0c1fe369452dd05051b73e9c0330a346c3eabd7c
Manikant <mani87y@gmail.com>
2024-11-07 22:59:55 +0530

    Your commit message here

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
+# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Install gunicorn explicitly
+pip install gunicorn
 
 # Collect static files
 python manage.py collectstatic --no-input
